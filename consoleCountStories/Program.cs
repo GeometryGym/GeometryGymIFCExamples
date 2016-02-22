dc38fa4c53cb2c3179f86631ed5b331873df0449
Jon Mirtschin <jonm@geometrygym.com>
2016-02-22 16:56:36 +1100

    Initial Commit

---
+++ consoleCountStories/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryGym.Ifc;
+
+namespace testConsole
+{
+	// type "C:\My Work\Geometry Gym\documents\GeometryGym\BIM\ifc\grasshopper\older\151119 klein slabs 7 stories.ifc" | testConsole.exe
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DatabaseIfc db = new DatabaseIfc(Console.In);
+			IfcProject project = db.Project;
+			IfcSpatialElement rootElement = project.RootElement;
+			int buildingStoreyCount = CountStories(rootElement);
+			Console.Out.Write("Number of Stories in file :" + buildingStoreyCount);
+		}
+
+		static int CountStories(IfcSpatialElement element)
+		{
+			if (element == null)
+				return 0;
+			int result = 0;
+			List<IfcRelAggregates> aggregates = element.IsDecomposedBy;
+			foreach(IfcRelAggregates rag in aggregates)
+			{
+				List<IfcObjectDefinition> objects = rag.RelatedObjects;
+				foreach(IfcObjectDefinition obj in objects)
+				{
+					IfcBuildingStorey storey = obj as IfcBuildingStorey;
+					if (storey != null)
+						result++;
+					else
+					{
+						IfcSpatialElement se = obj as IfcSpatialElement;
+						if (se != null)
+							result += CountStories(se);
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
---
+++ consoleCountStories/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("testConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Geometry Gym Pty Ltd")]
+[assembly: AssemblyProduct("testConsole")]
+[assembly: AssemblyCopyright("Copyright © Geometry Gym Pty Ltd 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9ea65a61-1ea1-4eff-b191-2fd68f170e97")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
