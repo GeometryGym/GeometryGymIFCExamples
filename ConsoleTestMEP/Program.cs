05de537c5947e2e8cc5f9c22b6f49a45fcf3747d
Jon Mirtschin <jonm@geometrygym.com>
2016-05-06 13:43:53 +1000

    Mep Examples

---
+++ ConsoleTestMEP/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+using GeometryGym.Ifc;
+
+namespace ConsoleTestMEP
+{
+	class Program
+	{
+		//Example files for https://sourceforge.net/p/ifcexporter/discussion/general/thread/9e08650b/
+
+		static void Main(string[] args)
+		{
+			DirectoryInfo di = Directory.GetParent(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+			di = Directory.GetParent(di.FullName);
+
+			string path = Path.Combine(di.FullName, "examples");
+			if (!Directory.Exists(path))
+				Directory.CreateDirectory(path);
+			Generate(TypeIFC.AirTerminal, Schema.IFC2x3, path);
+			Generate(TypeIFC.AirTerminal, Schema.IFC4A1, path);
+			Generate(TypeIFC.Chiller, Schema.IFC2x3, path);
+			Generate(TypeIFC.Chiller, Schema.IFC4A1, path);
+			Generate(TypeIFC.ElectricPoint, Schema.IFC2x3, path);
+
+		}
+		internal enum TypeIFC { Chiller, AirTerminal, ElectricPoint };
+		static void Generate(TypeIFC type, Schema schema, string path)
+		{
+			DatabaseIfc db = new DatabaseIfc(true,schema);
+			IfcBuilding building = new IfcBuilding(db, "IfcBuilding") { };
+			IfcProject project = new IfcProject(building, "IfcProject", IfcUnitAssignment.Length.Millimetre) { };
+
+			IfcRectangleProfileDef rect = new IfcRectangleProfileDef(db, "Rect", 1000, 500);
+			IfcExtrudedAreaSolid extrusion = new IfcExtrudedAreaSolid(rect, new IfcAxis2Placement3D(new IfcCartesianPoint(db, 0, 0, 0)), new IfcDirection(db, 0, 0, 1), 2000);
+			IfcProductDefinitionShape rep = new IfcProductDefinitionShape(new IfcShapeRepresentation(extrusion));
+
+			if (type == TypeIFC.Chiller)
+			{
+				IfcChiller chiller = new IfcChiller(building, null, rep, null) { PredefinedType = IfcChillerTypeEnum.AIRCOOLED };
+				if(schema == Schema.IFC2x3)
+					chiller.RelatingType = new IfcChillerType(db, "MyChillerType", IfcChillerTypeEnum.AIRCOOLED);
+			}
+			else if (type == TypeIFC.AirTerminal)
+			{
+				IfcAirTerminal terminal = new IfcAirTerminal(building, null, rep, null) { PredefinedType = IfcAirTerminalTypeEnum.DIFFUSER };
+				if (schema == Schema.IFC2x3)
+					terminal.RelatingType = new IfcAirTerminalType(db, "MyAirTerminalType", IfcAirTerminalTypeEnum.DIFFUSER);
+			}
+			else
+			{
+				IfcElectricDistributionPoint point = new IfcElectricDistributionPoint(building, null, rep, null) { DistributionPointFunction = IfcElectricDistributionPointFunctionEnum.DISTRIBUTIONBOARD };
+			}
+			db.WriteFile(Path.Combine(path, type.ToString() + " " + schema.ToString() + ".ifc"));
+		}
+	}
+}
